88cf2f01a8a1c6db2b9f57c2f5fc8b7f19c51093
cattia <you@example.com>
2020-04-07 08:18:47 -0400

    [cricri] initialisation projet

---
+++ AppWithPlugin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppWithPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
