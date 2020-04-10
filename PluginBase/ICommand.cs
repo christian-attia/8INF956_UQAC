7c284a87c7936cddcae90226130fb334c6f38a69
cattia <you@example.com>
2020-04-10 00:10:15 -0400

    [cricri] initialisation tutoriel

---
+++ PluginBase/ICommand.cs
@@ -0,0 +1,23 @@
+﻿namespace PluginBase
+{
+    public interface ICommand
+    {
+        
+        string Name { get; }
+        string Description { get; }
+
+
+        int Execute();
+
+        string[] pluginPaths = new string[]
+{
+    // Paths to plugins to load.
+};
+
+IEnumerable<ICommand> commands = pluginPaths.SelectMany(pluginPath =>
+{
+    Assembly pluginAssembly = LoadPlugin(pluginPath);
+    return CreateCommands(pluginAssembly);
+}).ToList();
+    }
+}
